b80768f2df7cff1a9b9d1fde083be028147441b1
silvio <sequincozes@gmail.com>
2022-07-05 10:11:09 -0300

    Código-desafio, parte 2.

--- src/main/java/avaliacao_pratica/Principal.java
+++ src/main/java/avaliacao_pratica/Principal.java
@@ -2,13 +2,14 @@ package avaliacao_pratica;
 
 import weka.classifiers.AbstractClassifier;
 import weka.classifiers.lazy.IBk;
+import weka.classifiers.trees.J48;
 import weka.core.Instance;
 import weka.core.Instances;
 
 // https://github.com/sequincozes/TES
 public class Principal {
 
-    public static double normalClass = 0;
+    public static double normalClass = 0; // isso aqui representa as classes com o valor N
 
     public static void main(String args[]) throws Exception {
         // Leitura de datasets para a memória
@@ -17,12 +18,12 @@ public class Principal {
 
 //        new avaliacao_pratica.FeatureSelection().rankFeatures(datasetTreinamento, 10);
 
-        int[] features = new int[]{1,17, 19};
-        datasetTreinamento = Auxiliar.selecionaFeatures(datasetTreinamento, features);
-        datasetTestes = Auxiliar.selecionaFeatures(datasetTestes, features);
+//        int[] features = new int[]{1,17, 19};
+//        datasetTreinamento = Auxiliar.selecionaFeatures(datasetTreinamento, features);
+//        datasetTestes = Auxiliar.selecionaFeatures(datasetTestes, features);
 
 //        // Construção do modelo de classificação (treinamento)
-        AbstractClassifier classificador = new IBk(); // nova instância de um classificador qualquer
+        AbstractClassifier classificador = new J48(); // nova instância de um classificador qualquer
         AbstractClassifier classificadorTreinado = Auxiliar.construir(datasetTreinamento, classificador);
 //
 //        // Testes e processamento de resultados
@@ -31,29 +32,29 @@ public class Principal {
 
     public static void processaResultados(AbstractClassifier classificador, Instances teste) {
         // Resultados
-        float VP = 0;
-        float VN = 0;
-        float FP = 0;
-        float FN = 0;
+        float VP = 0; // quando o IDS diz que está acontecendo um ataque, e realmente está
+        float VN = 0; // quando o IDS diz que NÃO está acontecendo um ataque, e realmente NÃO está
+        float FP = 0; // quando o IDS diz que está acontecendo um ataque, PORÉM NÃO ESTÁ
+        float FN = 0; // quando o IDS diz que NÃO está acontecendo um ataque, PORÉM ESTÁ!
         long beginNano = System.nanoTime();
 
         int[][] confusionMatrix = new int[2][2];
-        for (int i = 0; i < teste.size(); i++) {
+        for (int i = 0; i < teste.size(); i++) { //percorre cada uma das amostras de teste
             try {
                 Instance testando = teste.instance(i);
-                double resultado = classificador.classifyInstance(testando);
+                double resultado = Auxiliar.testarInstancia(classificador, testando);
                 double esperado = testando.classValue();
-                if (resultado == esperado) {
+                if (resultado == esperado) { // já sabemos que o resultado é verdadeiro
                     if (resultado == normalClass) {
-                        VN = VN + 1;
+                        VN = VN + 1; // O IDS diz que NÃO está acontecendo um ataque, e realmente NÃO está
                     } else {
-                        VP = VP + 1;
+                        VP = VP + 1; // o IDS diz que está acontecendo um ataque, e realmente está
                     }
-                } else { // bad prediction
+                } else { // sabemos que é um "falso"
                     if (resultado == normalClass) {
-                        FN = FN + 1;
+                        FN = FN + 1; // o IDS diz que NÃO está acontecendo um ataque, PORÉM ESTÁ!
                     } else {
-                        FP = FP + 1;
+                        FP = FP + 1; // o IDS diz que está acontecendo um ataque, PORÉM NÃO ESTÁ
                     }
                 }
 
@@ -75,9 +76,9 @@ public class Principal {
         System.out.println("     - Tempo de processamento por amostra: " + totalNano / teste.size() + " microssegundos");
 
         System.out.println(" ### Desempenho na classificação");
-        float acuracia = (VP + VN) * 100 / (VP + VN + FP + FN);
-        float recall = (VP * 100) / (FN + VP);
-        float precision = (VP * 100) / (VP + FP);
+        float acuracia = (VP + VN) * 100 / (VP + VN + FP + FN); // quantos acertos o IDS teve
+        float recall = (VP * 100) / (VP + FN); // quantas vezes eu acertei dentre as vezes REALMENTE ESTAVA acontecendo um ataque
+        float precision = (VP * 100) / (VP + FP); // quantas vezes eu acertei dentre as vezes que eu DISSE que estava acontecendo
         float f1score = 2 * (recall * precision) / (recall + precision);
         System.out.println("     - VP: " + VP + ", VN: " + VN + ", FP: " + FP + ", VN: " + FN);
         System.out.println("     - F1-Score: " + f1score + "%");
